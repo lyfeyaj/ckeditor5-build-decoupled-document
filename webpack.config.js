43625445271ec81e87b9861de5e4244723f301af
lyfeyaj <lyfeyaj@gmail.com>
2018-08-08 19:07:40 +0800

    rebuild ckeditor

--- webpack.config.js
+++ webpack.config.js
@@ -7,31 +7,32 @@
 
 /* eslint-env node */
 
-const path = require( 'path' );
-const webpack = require( 'webpack' );
-const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
-const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
-const UglifyJsWebpackPlugin = require( 'uglifyjs-webpack-plugin' );
+const path = require('path');
+const webpack = require('webpack');
+const {bundler, styles} = require('@ckeditor/ckeditor5-dev-utils');
+const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
+const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
 	devtool: 'source-map',
-	performance: { hints: false },
+	performance: {
+		hints: false
+	},
 
-	entry: path.resolve( __dirname, 'src', 'ckeditor.js' ),
+	entry: path.resolve(__dirname, 'src', 'ckeditor.js'),
 
 	output: {
 		// The name under which the editor will be exported.
 		library: 'DecoupledEditor',
 
-		path: path.resolve( __dirname, 'build' ),
+		path: path.resolve(__dirname, 'build'),
 		filename: 'ckeditor.js',
 		libraryTarget: 'umd',
 		libraryExport: 'default'
 	},
 
 	optimization: {
-		minimizer: [
-			new UglifyJsWebpackPlugin( {
+		minimizer: [new UglifyJsWebpackPlugin({
 				sourceMap: true,
 				uglifyOptions: {
 					output: {
@@ -39,30 +40,32 @@ module.exports = {
 						comments: /^!/
 					}
 				}
-			} )
-		]
+			})]
 	},
 
 	plugins: [
-		new CKEditorWebpackPlugin( {
+		new CKEditorWebpackPlugin({
 			// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
 			// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
 			language: 'en',
 			additionalLanguages: 'all'
-		} ),
-		new webpack.BannerPlugin( {
-			banner: bundler.getLicenseBanner(),
-			raw: true
-		} )
+		}),
+		new webpack.BannerPlugin({banner: bundler.getLicenseBanner(), raw: true})
 	],
 
 	module: {
 		rules: [
 			{
-				test: /\.svg$/,
-				use: [ 'raw-loader' ]
+				test: /\.js$/,
+				use: [{
+					loader: 'babel-loader',
+					options: { cacheDirectory: true, presets: ['es2015', 'react'] }
+				}]
 			},
 			{
+				test: /\.svg$/,
+				use: ['raw-loader']
+			}, {
 				test: /\.css$/,
 				use: [
 					{
@@ -70,16 +73,15 @@ module.exports = {
 						options: {
 							singleton: true
 						}
-					},
-					{
+					}, {
 						loader: 'postcss-loader',
-						options: styles.getPostCssConfig( {
+						options: styles.getPostCssConfig({
 							themeImporter: {
-								themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
+								themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
 							},
 							minify: true
-						} )
-					},
+						})
+					}
 				]
 			}
 		]
